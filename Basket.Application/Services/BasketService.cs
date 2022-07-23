f44c8721fe388a9925ac477b3bf6dc25fbdc74a5
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-07-23 21:49:48 +0300

    Basket servis yazıldı

---
+++ Basket.Application/Services/BasketService.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Basket.Application.Requests;
+using Basket.Domain.Contracts;
+
+namespace Basket.Application.Services
+{
+    public class BasketService:IBasketService
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketService(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        public async Task CreateBasket(CreateBasketApplicationRequest request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.CreateBasket(request.UserId, cancellationToken);
+        }
+
+        public async Task ClearBasket(ClearBasketApplicationRequest request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.ClearBasket(request.BasketId, cancellationToken);
+        }
+
+        public async Task UpdateBasketItem(UpdateBasketItemApplicationRequest request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.UpdateBasketItem(request.BasketId, request.ProductId, request.Quantity,
+                cancellationToken);
+        }
+
+        public async Task AddBasketItem(AddBasketItemApplicationRequest request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.AddBasketItem(request.BasketId,request.ProductId,request.Price,request.Quantity,cancellationToken);
+        }
+
+        public async Task RemoveBasketItem(RemoveBasketItemApplicationRequest request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.RemoveBasketItem(request.BasketId, request.ProductId, cancellationToken);
+        }
+
+        public async Task<Domain.Basket> GetBasket(GetBasketApplicationRequest request, CancellationToken cancellationToken)
+        {
+            return await _basketRepository.GetBasket(request.BasketId, cancellationToken);
+        }
+    }
+}
---
+++ Basket.Application/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Basket.Application.Requests;
+
+namespace Basket.Application.Services
+{
+    public interface IBasketService
+    {
+        Task CreateBasket(CreateBasketApplicationRequest request, CancellationToken cancellationToken);
+        Task ClearBasket(ClearBasketApplicationRequest request,CancellationToken cancellationToken);
+        Task UpdateBasketItem(UpdateBasketItemApplicationRequest request, CancellationToken cancellationToken);
+        Task AddBasketItem(AddBasketItemApplicationRequest request, CancellationToken cancellationToken);
+        Task RemoveBasketItem(RemoveBasketItemApplicationRequest request, CancellationToken cancellationToken);
+        Task<Domain.Basket> GetBasket(GetBasketApplicationRequest request, CancellationToken cancellationToken);
+    }
+}
