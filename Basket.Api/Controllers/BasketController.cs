cdf53a8e3334d787112eb75802ca600dc2057f5d
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-07-23 21:51:02 +0300

    Requestler mapplendi,Controllera methodlar yazıldı

---
+++ Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Basket.Api.Extensions;
+using Basket.Application.Services;
+using Basket.Definition.Request;
+using Basket.Domain;
+using Basket.Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketController:ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        
+        [HttpPost]
+        [Route("createBasket")]
+        public async Task<IActionResult> CreateBasket(CreateBasketApiRequest request, CancellationToken cancellationToken)
+        {
+            await _basketService.CreateBasket(request.ToApplicationRequest(), cancellationToken);
+
+            return Ok();
+        }
+        [HttpPost]
+        [Route("addBasketItem")]
+        public async Task<IActionResult> AddBasketItem(AddBasketItemApiRequest request, CancellationToken cancellationToken)
+        {
+            await _basketService.AddBasketItem(request.ToApplicationRequest(), cancellationToken);
+
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("removeBasketItem")]
+        public async Task<IActionResult> RemoveBasketItem(RemoveBasketItemApiRequest request,
+            CancellationToken cancellationToken)
+        {
+            await _basketService.RemoveBasketItem(request.ToApplicationRequest(), cancellationToken);
+            
+            return Ok();
+        }
+        [HttpPost]
+        [Route("clearBasket")]
+        public async Task<IActionResult> ClearBasket(ClearBasketApiRequest request, CancellationToken cancellationToken)
+        {
+            await _basketService.ClearBasket(request.ToApplicationRequest(), cancellationToken);
+
+            return Ok();
+        }
+        [HttpPost]
+        [Route("updateBasketItem")]
+        public async Task<IActionResult> UpdateBasketItem(UpdateBasketItemApiRequest request, CancellationToken cancellationToken)
+        {
+            await _basketService.UpdateBasketItem(request.ToApplicationRequest(), cancellationToken);
+
+            return Ok();
+        }
+        [HttpGet]
+        [Route("getBasket")]
+        public async Task<IActionResult> GetBasket([FromQuery]GetBasketApiRequest request, CancellationToken cancellationToken)
+        {
+            await _basketService.GetBasket(request.ToApplicationRequest(), cancellationToken);
+
+            return Ok();
+        }
+    }
+}
--- Basket.Api/Extensions/MappingExtensions.cs
+++ Basket.Api/Extensions/MappingExtensions.cs
@@ -47,5 +47,53 @@ namespace Basket.Api.Extensions
                 UserId = request.UserId
             };
         }
+        public static AddBasketItemApplicationRequest ToApplicationRequest(this AddBasketItemApiRequest request)
+        {
+            return new AddBasketItemApplicationRequest()
+            {
+                BasketId = request.BasketId,
+                ProductId = request.ProductId,
+                Price = request.Price,
+                Quantity = request.Quantity
+            };
+        }
+        public static ClearBasketApplicationRequest ToApplicationRequest(this ClearBasketApiRequest request)
+        {
+            return new ClearBasketApplicationRequest()
+            {
+                BasketId = request.BasketId
+            };
+        }
+        public static CreateBasketApplicationRequest ToApplicationRequest(this CreateBasketApiRequest request)
+        {
+            return new CreateBasketApplicationRequest()
+            {
+                UserId = request.UserId
+            };
+        }
+        public static GetBasketApplicationRequest ToApplicationRequest(this GetBasketApiRequest request)
+        {
+            return new GetBasketApplicationRequest()
+            {
+                BasketId = request.BasketId
+            };
+        }
+        public static RemoveBasketItemApplicationRequest ToApplicationRequest(this RemoveBasketItemApiRequest request)
+        {
+            return new RemoveBasketItemApplicationRequest()
+            {
+                BasketId = request.BasketId,
+                ProductId = request.ProductId
+            };
+        }
+        public static UpdateBasketItemApplicationRequest ToApplicationRequest(this UpdateBasketItemApiRequest request)
+        {
+            return new UpdateBasketItemApplicationRequest()
+            {
+                BasketId = request.BasketId,
+                ProductId = request.ProductId,
+                Quantity = request.Quantity
+            };
+        }
     }
 }
